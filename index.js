db73fea6bcfb4069e491a934d4949a6a036f7d1d
leMaik <an@maik.tk>
2015-11-27 22:45:15 +0100

    (fix) Make task description markdown work.

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var ko = require('knockout');
 var _ = require('lodash');
 var cuid = require('cuid');
-var markdown = require('./src/markdown')();
+var md = require('./src/markdown');
 var fs = require('fs');
 
 var ViewModel = function(params) {
-  console.log(cuid());
   this.descriptionId = cuid();
   this.solutionId = cuid();
   this.task = params.task;
@@ -13,9 +12,33 @@ var ViewModel = function(params) {
 };
 
 ViewModel.prototype.init = function(element) {
-  //markdown()(this.descriptionId).render(this.task.text());
+  md()(this.descriptionId).render(this.task.text());
+
   if (this.showSolutionPreview) {
     //TODO render solution
+    createPreview = md({
+      testProcessor: {
+        register: function(name) {
+          if (lastEdit > curEdit)
+            return;
+          curTests[taskIdx].push({name: name, passes: false})
+        },
+        testResult: function(err, idx) {
+          if (lastEdit > curEdit)
+            return;
+          curTests[taskIdx][idx].passes = (err == null)
+        },
+        testsFinished: function() {
+          if (lastEdit > curEdit)
+            return;
+          allTests(curTests);
+        },
+        template: function() { return ""; }
+      }
+    });
+
+    prev = createPreview(this.solutionId)
+    prev.render(this.task.tests() + "\n\n" + this.task.solution)
   }
 };
 
