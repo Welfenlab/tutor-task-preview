2cabdba44eaea6cf1c154c90320c9046bf0bbfc7
leMaik <an@maik.tk>
2015-12-05 01:55:46 +0100

    (fix) Make double buffering and test order checking work together.

--- src/PreviewRendererFactory.js
+++ src/PreviewRendererFactory.js
@@ -3,60 +3,61 @@ var _ = require('lodash');
 
 var PreviewRendererFactory = function (id, setTestResults) {
   var lastEdit = 0
+  var curTests = {}
+  var renderer;
 
-  var renderer = function (curEdit) {
-    if (setTestResults) {
-      var sboxDisconnect = null
-      var curTests = {}
+  if (setTestResults) {
+    var sboxDisconnect = null
 
-      return md({
-        testProcessor: {
-          init: function (disconnect) {
-            if(sboxDisconnect && typeof(sboxDisconnect) == "function"){
-              sboxDisconnect()
-            }
-            sboxDisconnect = disconnect;
-          },
-          register: function(name) {
-            if (lastEdit <= curEdit) {
-              curTests[name] = {
-                name: name,
-                status: "running",
-                passes: undefined
-              };
-            }
-          },
-          testResult: function(err, name) {
-            if (lastEdit <= curEdit) {
-              curTests[name].passes = (err == null);
-              curTests[name].error = err;
-            }
-          },
-          testsFinished: function(err) {
-            if (lastEdit <= curEdit) {
-              setTestResults(_.map(curTests, function(t){
-                if (t.status == "running") {
-                  t.status = err
-                }
-                if (!t.passes /* undefined or false */) {
-                  t.passes = false
-                }
-                return t
-              }));
-            }
-          },
-          template: function() { return ""; }
-        }
-      })(id);
-    } else {
-      return md()(id);
-    }
-  };
+    renderer = md({
+      testProcessor: {
+        init: function (disconnect) {
+          if(sboxDisconnect && typeof(sboxDisconnect) == "function"){
+            sboxDisconnect()
+          }
+          sboxDisconnect = disconnect;
+        },
+        register: function(name) {
+          if (lastEdit <= renderer.__curEdit) {
+            curTests[name] = {
+              name: name,
+              status: "running",
+              passes: undefined
+            };
+          }
+        },
+        testResult: function(err, name) {
+          if (lastEdit <= renderer.__curEdit) {
+            curTests[name].passes = (err == null);
+            curTests[name].error = err;
+          }
+        },
+        testsFinished: function(err) {
+          if (lastEdit <= renderer.__curEdit) {
+            setTestResults(_.map(curTests, function(t){
+              if (t.status == "running") {
+                t.status = err
+              }
+              if (!t.passes /* undefined or false */) {
+                t.passes = false
+              }
+              return t
+            }));
+          }
+        },
+        template: function() { return ""; }
+      }
+    })(id);
+  } else {
+    renderer = md()(id);
+  }
 
   return {
     render: function() {
+      curTests = {}
       lastEdit++;
-      return renderer(lastEdit).render.apply(null, arguments);
+      renderer.__curEdit = lastEdit;
+      return renderer.render.apply(null, arguments);
     }
   };
 };
