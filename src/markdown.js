4bcfbb90e1130a9ea1297c455ad1d0f8fd34c451
Alexander Treptau <atreptau@welfenlab.de>
2015-12-07 14:50:09 +0100

    (feature) Add support for graph-charts and fix tree-processor + formating

--- src/markdown.js
+++ src/markdown.js
@@ -1,7 +1,7 @@
-var fs = require('fs');
-var path = require('path');
-var _ = require('lodash');
-var tutorMarkdown = require('@tutor/markdown2html');
+var fs = require('fs')
+var path = require('path')
+var _ = require('lodash')
+var tutorMarkdown = require('@tutor/markdown2html')
 
 var defaultConfig = {
   runTimeout: 1.5 * 1000,
@@ -10,21 +10,28 @@ var defaultConfig = {
     template: _.template(fs.readFileSync(path.join(__dirname, 'js_controls.html'), 'utf8'))
   },
   dotProcessor: {
-    baseSVGTemplate: _.template("<svg data-element-id=\"<%= id %>\"><g/></svg>"),
+    baseSVGTemplate: _.template('<svg data-element-id="<%= id %>"><g/></svg>'),
     errorTemplate: _.template("<p style='background-color:red'><%= error %></p>")
   },
   testProcessor: {
     registerTest: _.noop,
     testResult: _.noop,
     testsFinished: _.noop,
-    template: _.template("<h1>Tests</h1><ul data-element-id=\"<%= id %>\"></ul>")
+    template: _.template('<h1>Tests</h1><ul data-element-id="<%= id %>"></ul>')
   },
-  treeProcessor: false
-};
+  treeProcessor: {
+    baseSVGTemplate: _.template('<svg data-element-id="<%= id %>"></svg>'),
+    errorTemplate: _.template("<p style='background-color:red'><%= error %></p>")
+  },
+  graphProcessor: {
+    baseSVGTemplate: _.template('<div data-element-id="<%= id %>"><svg></svg></div>'),
+    errorTemplate: _.template("<p style='background-color:red'><%= error %></p>")
+  }
+}
 
-module.exports = function(config) {
-  var localConf;
-  config = config || {};
-  localConf = _.defaultsDeep({}, config, defaultConfig);
-  return tutorMarkdown(localConf);
-};
+module.exports = function (config) {
+  var localConf
+  config = config || {}
+  localConf = _.defaultsDeep({}, config, defaultConfig)
+  return tutorMarkdown(localConf)
+}
